1a1ccf0b67380ae33ee12bb83d8db8e0a9dd3c98
Pete Benoit <peterbenoit@gmail.com>
2024-10-25 10:30:53 -0400

    Update App.jsx
    - using as variable

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -5,12 +5,13 @@ function getRandomReactDescription() {
 }
 
 function Header() {
+	const description = getRandomReactDescription();
 	return (
 		<header>
 		<img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
 		<h1>React Essentials</h1>
 		<p>
-			{getRandomReactDescription()} React concepts you will need for almost any app you are
+			{description} React concepts you will need for almost any app you are
 			going to build!
 		</p>
 		</header>
