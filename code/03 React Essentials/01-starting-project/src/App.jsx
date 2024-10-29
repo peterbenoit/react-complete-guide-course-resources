e3bc64a9e037e2ded9a5284d652b736ee8803365
Pete Benoit <peterbenoit@gmail.com>
2024-10-29 14:46:39 -0400

    Creating CoreConcepts component file

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,33 +1,10 @@
 
 import imgComponent from "./assets/components.png";
 import imgComponent1 from "./assets/state-mgmt.png";
-
-import { CORE_CONCEPTS } from "./data";
-
 import Header from "./components/Header";
+import CoreConcepts from "./components/CoreConcepts";
 
 
-// destructuring props
-// function CoreConcepts( { title, description, image } ) {
-// 	return (
-// 		<li>
-// 			<h2>{title}</h2>
-// 			<img src={image} alt="Components" />
-// 			<p>{description}</p>
-// 		</li>
-// 	);
-// }
-
-// using 'props' as the object to derive the values, but it can be any word
-function CoreConcepts(props) {
-	return (
-		<li>
-			<h2>{props.title}</h2>
-			<img src={props.image} alt="Components" />
-			<p>{props.description}</p>
-		</li>
-	);
-}
 
 function App() {
 	return (
---
+++ code/03 React Essentials/01-starting-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,23 @@
+import { CORE_CONCEPTS } from "../data";
+
+// destructuring props
+// function CoreConcepts( { title, description, image } ) {
+// 	return (
+// 		<li>
+// 			<h2>{title}</h2>
+// 			<img src={image} alt="Components" />
+// 			<p>{description}</p>
+// 		</li>
+// 	);
+// }
+
+// using 'props' as the object to derive the values, but it can be any word
+function CoreConcepts(props) {
+	return (
+		<li>
+			<h2>{props.title}</h2>
+			<img src={props.image} alt="Components" />
+			<p>{props.description}</p>
+		</li>
+	);
+}
