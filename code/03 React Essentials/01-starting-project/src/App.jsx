2c357953cd84f8d3f791824e1632a966fa517aa2
Pete Benoit <peterbenoit@gmail.com>
2024-10-29 14:44:02 -0400

    moving header component into its own file

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,28 +1,11 @@
-import img from "./assets/react-core-concepts.png";
+
 import imgComponent from "./assets/components.png";
 import imgComponent1 from "./assets/state-mgmt.png";
 
 import { CORE_CONCEPTS } from "./data";
 
-const reactDescriptions = ["Super", "Awesome", "Cool", "Good", "Great"];
+import Header from "./components/Header";
 
-function getRandomReactDescription() {
-	return reactDescriptions[Math.floor(Math.random() * reactDescriptions.length)];
-}
-
-function Header() {
-	const description = getRandomReactDescription();
-	return (
-		<header>
-		<img src={img} alt="Stylized atom" />
-		<h1>React Essentials</h1>
-		<p>
-			{description} React concepts you will need for almost any app you are
-			going to build!
-		</p>
-		</header>
-	);
-}
 
 // destructuring props
 // function CoreConcepts( { title, description, image } ) {
---
+++ code/03 React Essentials/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import img from "../assets/react-core-concepts.png";
+const reactDescriptions = ["Super", "Awesome", "Cool", "Good", "Great"];
+
+function getRandomReactDescription() {
+	return reactDescriptions[Math.floor(Math.random() * reactDescriptions.length)];
+}
+
+export default function Header() {
+	const description = getRandomReactDescription();
+	return (
+		<header>
+		<img src={img} alt="Stylized atom" />
+		<h1>React Essentials</h1>
+		<p>
+			{description} React concepts you will need for almost any app you are
+			going to build!
+		</p>
+		</header>
+	);
+}
