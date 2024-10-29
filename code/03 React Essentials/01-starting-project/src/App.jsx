1db3e518797ed92732b157b512384d661749e7bb
Pete Benoit <peterbenoit@gmail.com>
2024-10-29 14:01:57 -0400

    Update App.jsx
    - importing an image and referencing it

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import img from "./assets/react-core-concepts.png";
+
 const reactDescriptions = ["Super", "Awesome", "Cool", "Good", "Great"];
 
 function getRandomReactDescription() {
@@ -8,7 +10,7 @@ function Header() {
 	const description = getRandomReactDescription();
 	return (
 		<header>
-		<img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+		<img src={img} alt="Stylized atom" />
 		<h1>React Essentials</h1>
 		<p>
 			{description} React concepts you will need for almost any app you are
